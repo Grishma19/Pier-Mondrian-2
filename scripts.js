07b9a847809c8c79c43803287f1d91a3407a96eb
Grishma19 <91224283+Grishma19@users.noreply.github.com>
2021-10-06 13:53:52 -0400

    Files

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function setup(){
+  createCanvas(800,1000);
+  background('fffffff');
+}
+
+function draw(){
+  
+  var colors = ["red", "green", "white", "blue", "black", "yellow"];
+  var sizes = [700, 900, 400, 400];
+  // strokeWeight(6);
+
+  var x = 0;
+  var y = 0;
+
+  var currentWidth = random(sizes);
+  var currentHeight = random(sizes);
+
+  while (y < height){
+    x = 0;
+    while (x < width){
+      fill (random (colors));
+      strokeWeight(6);
+      rect (x, y, currentWidth, currentHeight);
+      x = x + currentWidth;
+    }
+    y = y + currentHeight;
+  }
+
+}
